90d783698dbe58bc729d3cdf25172611d6cea3e2
itadreik4 <tadreik@gmail.com>
2018-11-17 14:46:06 -0500

    refactor variables to more concise android ktx, added dark mode, removed string concatenation.

--- app/src/main/java/com/sfjamedia/android/ContactActivity.kt
+++ app/src/main/java/com/sfjamedia/android/ContactActivity.kt
@@ -2,34 +2,41 @@ package com.sfjamedia.android
 
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_contact.*
 
 
 class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         super.onCreate(savedInstanceState)
+        val nightModeFlags = this.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_NO) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
         setContentView(R.layout.activity_contact)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(toolbar)
 
-        findViewById<MaterialButton>(R.id.send_message).setOnClickListener {
-            val nameString = findViewById<EditText>(R.id.name_text).text.toString()
-            val emailString = findViewById<EditText>(R.id.email_text).text.toString()
-            val messageString = findViewById<EditText>(R.id.message_text).text.toString()
+        send_message.setOnClickListener {
+            val nameString = name_text.text.toString()
+            val emailString = email_text.text.toString()
+            val messageString = message_text.text.toString()
 
             if (nameString.length < 2 || emailString.length < 6 || messageString.length < 2) {
-                Toast.makeText(this@ContactActivity, "Please fill out the form", Toast.LENGTH_SHORT).show()
+                Snackbar.make(it, "Please fill out the form", Snackbar.LENGTH_SHORT).show()
 
             } else if (emailString.contains("@")) {
                 if (emailString.startsWith("@") || emailString.endsWith("@")) {
-                    Toast.makeText(this@ContactActivity, "Please enter a valid email address", Toast.LENGTH_SHORT).show()
+                    Snackbar.make(it, "Please enter a valid email address", Snackbar.LENGTH_SHORT).show()
                 } else {
-                    val finalMessage = ("Name: " + nameString
-                            + "\nEmail: " + emailString
-                            + "\nMessage: " + messageString)
+                    val finalMessage = ("Name: $nameString"
+                            + "\nEmail: $emailString"
+                            + "\nMessage: $messageString")
                     val emailIntent = Intent(Intent.ACTION_SEND)
                     val aEmailList = arrayOf("inquiries@sfjamedia.com")
                     emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList)
@@ -40,7 +47,7 @@ class ContactActivity : AppCompatActivity() {
                 }
 
             } else {
-                Toast.makeText(this@ContactActivity, "Please enter a valid email", Toast.LENGTH_SHORT).show()
+                Snackbar.make(it, "Please enter a valid email", Snackbar.LENGTH_SHORT).show()
             }
         }
     }
